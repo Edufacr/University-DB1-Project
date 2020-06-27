f27315ef498cfac55c28281affa746799956c646
Eduardo <edumadrim@gmail.com>
2020-06-27 10:14:21 -0600

    added getDocTypeNames

---
+++ DataBaseScripts/Queries/SP_getDocTypeNames.sql
@@ -0,0 +1,25 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Eduardo Madrigal Marín
+-- Create date: 
+-- Description:	Gets all properties seen by a user
+-- =============================================
+
+-- TODO Input validations
+
+CREATE OR ALTER PROCEDURE SP_getDocTypeNames
+AS
+BEGIN
+    SET NOCOUNT ON;
+    BEGIN TRY
+        SELECT Id, Name FROM DB1P_ChargeConcepts;
+        RETURN @@ROWCOUNT;
+    END TRY
+    BEGIN CATCH
+        RETURN @@Error * -1
+    END CATCH
+END
+GO
