d7c7f3f8e0aec0421257e622919387eb9e9b1c07
Eduardo <edumadrim@gmail.com>
2020-07-06 01:04:46 -0600

    added insertConsumptionMov

--- DataBaseScripts/Tests/Test.sql
+++ DataBaseScripts/Tests/Test.sql
@@ -4,5 +4,9 @@ EXEC SP_XML_IdTypesFill
 EXEC SP_XML_MovTypeFill
 EXEC SP_XML_UsersFill
 EXEC SP_XML_OperationsFill
-
-SELECT * FROM DB1P_MovType
+-- SELECT * FROM activeProperties;
+-- DECLARE @CheckDate datetime;  
+-- SET @CheckDate = GETDATE(); 
+-- EXEC SP_insertConsumptionMov 1,9837074,100,"Test",@CheckDate;
+-- SELECT * FROM DB1P_ConsumptionMov;
+-- SELECT * FROM activeProperties;
