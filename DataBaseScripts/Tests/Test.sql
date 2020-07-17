d77f125d9d841664d17ccca4858117e7717ef297
Eduardo <edumadrim@gmail.com>
2020-07-16 19:45:34 -0600

    comment to test

--- DataBaseScripts/Tests/Test.sql
+++ DataBaseScripts/Tests/Test.sql
@@ -5,11 +5,12 @@ EXEC SP_XML_IdTypesFill
 EXEC SP_XML_MovTypeFill
 EXEC SP_XML_UsersFill
 EXEC SP_XML_OperationsFill
-
+--!! HAY QUE CORRER TRIGGERS DESPUES DEL DROP & CREATE !!
 INSERT INTO DB1P_Users
 VALUES('a','a',1,1)
 
 SELECT * FROM DB1P_ConsumptionMov
+    WHERE Id_Property = 45
 SELECT * FROM DB1P_Receipt
 SELECT * FROM DB1P_PaidReceipts
 SELECT * FROM DB1P_ReconnectionReceipt
