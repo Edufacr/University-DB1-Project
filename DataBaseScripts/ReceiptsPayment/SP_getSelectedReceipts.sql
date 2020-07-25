065dfc3fe585548de98e3b6415492d7bc141a8e0
Eduardo <edumadrim@gmail.com>
2020-07-25 12:42:53 -0600

    Sp to querry selected receipts

--- DataBaseScripts/ReceiptsPayment/SP_getSelectedReceipts.sql
+++ DataBaseScripts/ReceiptsPayment/SP_getSelectedReceipts.sql
@@ -5,7 +5,7 @@ GO
 -- =============================================
 -- Author:		Eduardo Madrigal Marín
 -- Create date: 
--- Description: Querrys selected receipts (Single User Table)
+-- Description: Querrys selected receipts with moratory interest (Single User Table)
 -- =============================================
 CREATE OR ALTER PROCEDURE SP_getSelectedReceipts
 	-- Add the parameters for the stored procedure here
@@ -15,8 +15,32 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
     -- Insert statements for procedure here
+DECLARE @Date DATE;
+DECLARE @IdMoratoryInterestCC INT;
+DECLARE @NameMoratoryInterestCC VARCHAR(50);
 BEGIN TRY
-	RETURN -50003
+	SET @IdMoratoryInterestCC = 11; --MoratoryInterest ID from xml
+	SET @Date = GETDATE();
+	SELECT @NameMoratoryInterestCC = Name 
+		FROM DB1P_ChargeConcepts 
+			WHERE Id = @IdMoratoryInterestCC;
+	
+	SELECT NULL AS ReceiptNumber,@NameMoratoryInterestCC AS ChargeConceptName,@Date AS ReceiptDate,NULL AS DueDate,
+			Amount = (Amount*MoratoryInterestRate/365)* ABS(DATEDIFF(day,DueDate,@Date))
+        FROM activeReceiptsWithMoratoryRate ar
+            INNER JOIN DB1P_SelectedReceipts sr
+                ON ar.ReceiptNumber = sr.ReceiptNumber
+
+        WHERE ar.DueDate < @Date
+
+	UNION
+
+	SELECT sr.ReceiptNumber,cc.Name,Date,DueDate,Amount
+		FROM activeReceipts ar
+			INNER JOIN DB1P_ChargeConcepts cc
+				ON ar.Id_ChargeConcept = cc.Id
+			INNER JOIN DB1P_SelectedReceipts sr
+				ON sr.ReceiptNumber = ar.ReceiptNumber
 END TRY
 BEGIN CATCH
 	RETURN @@Error * -1
