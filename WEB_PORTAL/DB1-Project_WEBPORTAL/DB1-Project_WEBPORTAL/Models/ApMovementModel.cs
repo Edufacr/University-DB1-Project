ff6d913617d03a04cb28ca52fb334a462752caf8
Eduardo <edumadrim@gmail.com>
2020-08-01 10:26:45 -0600

    cambio de nombre en display de cuotas pendientes

--- WEB_PORTAL/DB1-Project_WEBPORTAL/DB1-Project_WEBPORTAL/Models/ApMovementModel.cs
+++ WEB_PORTAL/DB1-Project_WEBPORTAL/DB1-Project_WEBPORTAL/Models/ApMovementModel.cs
@@ -9,7 +9,7 @@ namespace DB1_Project_WEBPORTAL.Models
         [Required][Display (Name = "Tipo de Movimiento")] public string MovTypeName { set; get; }
         [Required][Display (Name = "Monto")] public double Amount { set; get; }
         [Required][Display (Name = "Interes Mensual")] public double MonthlyInterest { set; get; }
-        [Required][Display (Name = "Numero de cuotas pendientes")] public int PaymentTermsLeft { set; get; }
+        [Required][Display (Name = "Cuotas pendientes")] public int PaymentTermsLeft { set; get; }
         [Required][Display (Name = "Nuevo balance")] public double NewBalance { set; get; }
         [Required][Display (Name = "Fecha")] public DateTime Date { set; get; }
         [Required][Display (Name = "Fecha de creación")] public DateTime InsertedAt { set; get; }
