20abfc630a301d3dbba77c04dfca5a87cb32491e
Eduardo <edumadrim@gmail.com>
2020-07-31 22:46:42 -0600

    sp to get receipts of MovNumber

---
+++ DataBaseScripts/Queries/SP_getReceiptWithAp.sql
@@ -0,0 +1,38 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Eduardo Madrigal Marín
+-- Create date: 
+-- Description:	Gets details of receipt with AP
+-- =============================================
+CREATE OR ALTER PROCEDURE SP_getReceiptWithAp
+	-- Add the parameters for the stored procedure here
+    @inMovNumber INT
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+    -- Insert statements for procedure here
+    DECLARE @IdReceipt int;
+    DECLARE @ApCCName VARCHAR(50);
+BEGIN TRY
+    SELECT @IdReceipt = ReceiptNumber
+        FROM completeAPReceipts;
+    SELECT @ApCCName = Name
+        FROM completeCCs
+        WHERE Id = 12;
+    IF(@IdReceipt IS NOT NULL)
+    BEGIN
+        SELECT ReceiptNumber,@ApCCName,Amount,DueDate,Date AS ReceiptDate
+            FROM completeAPReceipts
+    END
+    RETURN -50002
+END TRY
+BEGIN CATCH
+	return @@Error * -1
+END CATCH
+END
+GO
